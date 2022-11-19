970de85728d3df50d4bae70d349b95b302dea36f
VanessaLin9 <vanessa7591@gmail.com>
2022-11-20 00:20:29 +0800

    938 DFS

---
+++ problem/0938_RangeSumofBST.js
@@ -0,0 +1,28 @@
+// 938. Range Sum of BST
+// https://leetcode.com/problems/range-sum-of-bst/description/
+// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+  const isInBetween = (val) => val >= low && val <= high;
+  const add = (val, sum) => isInBetween(val)? sum += val : sum;
+  const preOrder = (root, sum) => {
+    if(!root) return sum;
+    return add(root.val, sum) + preOrder(root.left, sum) + preOrder(root.right, sum)
+  } 
+  return preOrder(root, 0)   
+};
+
