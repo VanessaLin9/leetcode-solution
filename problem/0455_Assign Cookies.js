eb61f47ef9d021de2b0a9794267262f2726928b9
VanessaLin9 <vanessa7591@gmail.com>
2022-12-06 21:34:21 -0500

    455 greedy

---
+++ problem/0455_Assign Cookies.js
@@ -0,0 +1,24 @@
+//Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+
+// Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let content = 0;
+  let result = 0;
+  for(let i of s){
+    if(i >= g[content]) {
+      content ++
+      result ++
+    }
+  }
+  return result;
+};
+
+console.log(findContentChildren([1,2,3], [1,1]))
