0a689fd1d86509e75476f5e4d0af83cdd01ad201
VanessaLin9 <vanessa7591@gmail.com>
2022-12-22 14:51:00 -0500

    binary search

---
+++ problem/0278_First Bad Version.js
@@ -0,0 +1,42 @@
+// 278. First Bad Version
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+// You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+      let left = 0;
+      let right = n;
+
+      while(right - left !== 1) {
+        let mid = Math.floor((right + left)/2)
+
+        if(isBadVersion(mid)){
+          right = mid
+        } else {
+          left = mid
+        }
+      }
+      return right
+    };
+};
