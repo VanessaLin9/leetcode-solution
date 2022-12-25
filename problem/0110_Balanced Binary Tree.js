83dddd91231fba7de9e0993d4adc412e78bf32a8
VanessaLin9 <vanessa7591@gmail.com>
2022-12-25 18:14:05 -0500

    dfs postorder

--- problem/0110_Balanced Binary Tree.js
+++ problem/0110_Balanced Binary Tree.js
@@ -26,3 +26,19 @@ var isBalanced = function(root) {
 };
 // T: O(n^2)
 
+// postOrder
+var isBalanced = function(root) {
+  if(!root) return true;
+
+  let dfs = (node) => {
+    if(!node) return 0;
+    let left = 1 + dfs(node.left);
+    let right = 1 + dfs(node.right);
+
+    if(Math.abs(left - right) > 1) return Infinity;
+    return Math.max(left, right); 
+  }
+  return dfs(root) === Infinity? false: true;
+};
+
+// T: O(n)
