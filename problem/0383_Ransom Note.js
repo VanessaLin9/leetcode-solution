3a5a91db363aea648b27c30fc500ddc3fccd731c
VanessaLin9 <vanessa7591@gmail.com>
2022-12-24 22:00:52 -0500

    counter

---
+++ problem/0383_Ransom Note.js
@@ -0,0 +1,27 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+
+var canConstruct = function(ransomNote, magazine) {
+  let hash = {};
+  for(let i in magazine){
+    if(hash[magazine[i]] === undefined) hash[magazine[i]] = 0;
+    hash[magazine[i]]++
+  }
+  for(let j in ransomNote) {
+    if(hash[ransomNote[j]] > 0) {
+      hash[ransomNote[j]] --
+    } else {
+      return false
+    }
+  }
+  return true
+};
+
+console.log(canConstruct("ab","aa"))
