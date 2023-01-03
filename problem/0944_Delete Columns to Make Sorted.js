2076170db04f07c7e552f2fa3c03c2991fa847fc
VanessaLin9 <vanessa7591@gmail.com>
2023-01-03 11:15:55 -0500

    brute force

---
+++ problem/0944_Delete Columns to Make Sorted.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/delete-columns-to-make-sorted/
+
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+
+
+var minDeletionSize = function(strs) {
+  let result = 0;
+  for(let i=0; i<strs[0].length; i++){
+    for(let j=1; j<strs.length; j++){
+      if(strs[j-1][i] > strs[j][i]) {
+        result += 1;
+        break
+      }
+    }
+  }
+  return result
+};
+
+console.log(minDeletionSize(["cba","daf","ghi"]))
