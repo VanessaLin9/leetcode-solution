b13260bfc647ba64e1708a99263eb6e12d829cf4
VanessaLin9 <vanessa7591@gmail.com>
2023-01-18 16:17:45 -0500

    Destructuring assignment

---
+++ problem/0226_Invert Binary Tree.js
@@ -0,0 +1,18 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(root === null) return root;
+    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+    return root;
+};
