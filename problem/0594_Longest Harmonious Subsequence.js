cd4992b39775e8a2537deb26aa7bda6a53713bf1
VanessaLin9 <vanessa7591@gmail.com>
2023-03-02 17:14:16 +0800

    brust force

---
+++ problem/0594_Longest Harmonious Subsequence.js
@@ -0,0 +1,31 @@
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+// A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function(nums) {
+    nums.sort((a, b)=> b-a)
+    let max = nums[0]
+    const hash = {}
+    for(let i=0; i<nums.length; i++){
+        if(hash[nums[i]] === undefined){
+            hash[nums[i]] = 0
+        }
+        hash[nums[i]] ++
+    }
+    // console.log(hash)
+    let result = 0;
+    for(let j in hash){
+      if(hash[j-1] !== undefined){
+        let temp = hash[j] + hash[j-1]
+        result = temp>result?temp:result
+      }
+    }
+    return result
+};
+
+console.log(findLHS([-1,0,-1,0,-1,0,-1])) //7
