82349ed74b3898ffe64b28dfadc04f82c64d0f90
VanessaLin9 <vanessa7591@gmail.com>
2023-03-05 21:15:25 +0800

    burst force

---
+++ problem/1470_Shuffle the Array.js
@@ -0,0 +1,18 @@
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const result = [];
+    for(let i=0; i<n; i++){
+        result.push(nums[i])
+        result.push(nums[i+n])
+    }
+    return result
+};
+
+console.log(shuffle([2,5,1,3,4,7])) //[2,3,5,4,1,7]
