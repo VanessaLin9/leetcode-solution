4710601d0837182a7461730c4298d5d77b47530d
VanessaLin9 <vanessa7591@gmail.com>
2023-03-05 18:33:47 +0800

    regex

---
+++ problem/1108_Defanging an IP Address.js
@@ -0,0 +1,11 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    return address.replace(/\./g,"[.]" )
+};
