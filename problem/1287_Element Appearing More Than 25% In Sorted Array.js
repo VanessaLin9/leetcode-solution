98595acce96ee3d3227b569593c292109d8a61d9
VanessaLin9 <vanessa7591@gmail.com>
2023-03-06 14:06:50 +0800

    sliding window

---
+++ problem/1287_Element Appearing More Than 25% In Sorted Array.js
@@ -0,0 +1,16 @@
+// Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+// sliding window
+var findSpecialInteger = function(arr) {
+  const wd = Math.floor(arr.length/4)
+  for(let i=0; i<arr.length -wd; i++){
+    if(arr[i] === arr[i+wd]) return arr[i]
+  }
+  return -1
+};
+
+console.log(findSpecialInteger([1,2,3,4,5,6,7,8,9,10,11,12,12,12,12])) //12
