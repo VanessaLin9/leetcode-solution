7a91e51e7cc6b63a52e290761cf8b8cfca1c981c
VanessaLin9 <vanessa7591@gmail.com>
2023-03-12 16:24:42 +0800

    recursion

--- problem/1071_Greatest Common Divisor of Strings.js
+++ problem/1071_Greatest Common Divisor of Strings.js
@@ -7,11 +7,24 @@
  * @param {string} str2
  * @return {string}
  */
-var gcdOfStrings = function(str1, str2) {
-  if(str1 + str2 !== str2 + str1) return '';
-  const gcd = (a, b) => (b === 0?a: gcd(b, a%b))
-  return str1.slice(0, gcd(str1.length, str2.length))
-};
+// var gcdOfStrings = function(str1, str2) {
+//   if(str1 + str2 !== str2 + str1) return '';
+//   const gcd = (a, b) => (b === 0?a: gcd(b, a%b))
+//   console.log(gcd(str1.length, str2.length))
+//   return str1.slice(0, gcd(str1.length, str2.length))
+// };
+
+var  gcdOfStrings = function(str1, str2) {
+  if(str1+str2 !== str2+str1) {
+    return ""
+  } else if (str1 === str2){
+    return str1
+  } else if (str1.length > str2.length){
+     return gcdOfStrings(str1.substring(0, str2.length), str2)
+  } else if (str2.length > str1.length){
+    return gcdOfStrings(str1, str2.substring(0, str1.length))
+  }
+}
 
 console.log(gcdOfStrings("ABCABC", "ABC"))
 console.log(gcdOfStrings("ABABAB", "AB"))
