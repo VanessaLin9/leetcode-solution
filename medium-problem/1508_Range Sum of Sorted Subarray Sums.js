11e5236f5907da58763b4275363136dee409f65c
VanessaLin9 <vanessa7591@gmail.com>
2023-03-15 19:13:06 +0800

    minHeap but uncomplete

--- medium-problem/1508_Range Sum of Sorted Subarray Sums.js
+++ medium-problem/1508_Range Sum of Sorted Subarray Sums.js
@@ -9,13 +9,6 @@
  * @param {number} right
  * @return {number}
  */
-/**
- * @param {number[]} nums
- * @param {number} n
- * @param {number} left
- * @param {number} right
- * @return {number}
- */
 
 // brute force
 var rangeSum = function(nums, n, left, right) {
@@ -32,7 +25,54 @@ var rangeSum = function(nums, n, left, right) {
     for(let k=left-1; k<right; k++){
         result += arr[k]
     }
+    console.log(arr)
     return result % (Math.pow(10,9)+7);
 };
+// TC: n^2
+// MC: n^2
+// --------------------------------------
+// [min heap] 還是錯的沒有完成
+// var rangeSum = function(nums, n, left, right) {
+//     let heapSize = nums.length -1;
+//     function buildMinHeap(){
+//         for(let i=Math.floor(heapSize/2); i>=0; i--){
+//             minHeapify(i)
+//         }
+//     }
+//     function minHeapify(i){
+//         let min;
+//         let l = i * 2 + 1;
+//         let r = i * 2 + 2;
+//         if(l <= heapSize && nums[l] < nums[i]){
+//             min = l
+//         } else {
+//             min = i
+//         }
+//         if(r <= heapSize && nums[r] < nums[min]){
+//             min = r
+//         }
+//         if(min !== i){
+//             let temp = nums[i]
+//             nums[i] = nums[min]
+//             nums[min] = temp
+//             minHeapify(min)
+//         }
+//     }
+//     buildMinHeap()
+//     let result = 0
+//     let sumArr = []
+// 
+//     for(let k=0; k<right; k++){
+//       sumArr.push(nums[0])
+//       nums[0] = nums[0] + nums[1]  // 這邊有很大的問題
+//       minHeapify(0)
+//     }
+//     console.log(sumArr)
+//     for(let m=left-1; m<sumArr.length; m++){
+//       result += sumArr[m]
+//     }
+//     return result % (Math.pow(10, 9) + 7)
+// };
 
-console.log(rangeSum([1, 2, 3, 4], 4, 1, 5)) //13
+console.log(rangeSum([4, 3, 1, 2], 4, 1, 5)) //13
+console.log(rangeSum([1, 2, 3, 4], 4, 1, 10)) //13
