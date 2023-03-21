ee7cab914d7a5801c97143f6494dd6ba99cf0188
VanessaLin9 <vanessa7591@gmail.com>
2023-03-21 11:23:43 +0800

    single loop and hashSet solution

--- problem/0409_Longest Palindrome.js
+++ problem/0409_Longest Palindrome.js
@@ -9,27 +9,56 @@
 
 
 // brute force
-var longestPalindrome = function(s) {
-  const counter = {}
-  let result = 0;
-  for(let i in s){
-    if(counter[s[i]] === undefined) counter[s[i]] = 0
-    counter[s[i]] ++
-  }
+// var longestPalindrome = function(s) {
+//   const counter = {}
+//   let result = 0;
+//   for(let i in s){
+//     if(counter[s[i]] === undefined) counter[s[i]] = 0
+//     counter[s[i]] ++
+//   }
   
-  for(let j in counter) {
-    while(counter[j] >=2 ) {
-      counter[j] -= 2;
-      result += 2;
+//   for(let j in counter) {
+//     while(counter[j] >=2 ) {
+//       counter[j] -= 2;
+//       result += 2;
+//     }
+//   }
+//   for(let k in counter) {
+//     if(counter[k] === 1) {
+//       result += 1;
+//       break
+//     }
+//   }
+//   return result
+// };
+
+// brute force 濃縮成一個迴圈
+var longestPalindrome = function(s) {
+    const hash = {}
+    let result=0
+    for(let i=0; i<s.length; i++){
+        if(!hash[s[i]]) hash[s[i]] = 0;
+        hash[s[i]] ++
+        if(hash[s[i]] %2 === 0) result += 2;
     }
-  }
-  for(let k in counter) {
-    if(counter[k] === 1) {
-      result += 1;
-      break
-    }
-  }
-  return result
+    return result === s.length? s.length: result +1
 };
+
+// [hash set]
+// var longestPalindrome = function(s) {
+//     const set = new Set();
+//     let result =0;
+//     for(let i=0; i<s.length; i++){
+//         if(set.has(s[i])){
+//             result +=2
+//             set.delete(s[i])
+//         } else {
+//             set.add(s[i])
+//         }
+//     }
+//     return set.size === 0?s.length:result + 1;
+// };
+// TC: n
+// MC: n
 
 console.log(longestPalindrome("a"))
