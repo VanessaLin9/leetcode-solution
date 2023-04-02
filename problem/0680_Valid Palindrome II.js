9a91411191cee086b35a1778ad182c9a95f18d02
VanessaLin9 <vanessa7591@gmail.com>
2023-04-02 18:42:35 +0800

    easy to understand solution

--- problem/0680_Valid Palindrome II.js
+++ problem/0680_Valid Palindrome II.js
@@ -5,22 +5,37 @@
  * @return {boolean}
  */
 
-const validPalindrome = (s) => {
-    for (let i = 0, stop = s.length / 2; i < stop; i++) {
-        let j = s.length - i - 1
-        if (s[i] !== s[j]) {
-            return isPalindrome(cut(s, i)) || isPalindrome(cut(s, j))
-        }
+// const validPalindrome = (s) => {
+//     for (let i = 0, stop = s.length / 2; i < stop; i++) {
+//         let j = s.length - i - 1
+//         if (s[i] !== s[j]) {
+//             return isPalindrome(cut(s, i)) || isPalindrome(cut(s, j))
+//         }
+//     }
+//     return true
+// };
+
+// const cut = (s, i) => {
+//   // console.log(s.substring(0, i) + s.substring(i + 1))
+//   return s.substring(0, i) + s.substring(i + 1)
+// };
+
+// const isPalindrome = (s) => s === s.split('').reverse().join('');
+
+
+// 
+const validPalindrome = (str, skipped = false) => {
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] !== str[j]) {
+      if (skipped) return false;
+      return (
+        validPalindrome(str.substring(i, j), true) ||
+        validPalindrome(str.substring(i + 1, j + 1), true)
+      );
     }
-    return true
+  }
+  return true;
 };
-
-const cut = (s, i) => {
-  // console.log(s.substring(0, i) + s.substring(i + 1))
-  return s.substring(0, i) + s.substring(i + 1)
-};
-
-const isPalindrome = (s) => s === s.split('').reverse().join('');
 
 console.log(validPalindrome("deeee")) //true
 console.log(validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga")) //true
