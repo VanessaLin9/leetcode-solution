88b69d82fe1ab5a55a5acd3ec13138f2499b9576
VanessaLin9 <vanessa7591@gmail.com>
2023-04-03 11:37:34 +0800

    add some comment

--- medium-problem/0003_Longest Substring Without Repeating Characters.js
+++ medium-problem/0003_Longest Substring Without Repeating Characters.js
@@ -19,12 +19,34 @@ var lengthOfLongestSubstring = function (s) {
             left++;
         }
         set.add(s[i]);
-        maxSize = Math.max(maxSize, i - left + 1)
+        maxSize = Math.max(maxSize, i - left + 1) //i-left+1 可以直接用set.size 計算
     }
     return maxSize;
 }
 
 // TC: n
 // MC: n
+
+// 自己寫的比較差的
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set()
+    let ans = 0;
+    for(let i=0; i<s.length; i++){
+        set.add(s[i])
+        for(let j=i+1; j<s.length; j++){
+            if(set.has(s[j])) {
+                ans = Math.max(ans, set.size)
+                set.clear() //清空set的語法
+                break;
+            }
+            set.add(s[j])
+        }
+        ans = Math.max(ans, set.size)
+    }
+    return ans
+};
+// TC: n^2
+// MC: n
+// -----------------------------------------------
 
 console.log(lengthOfLongestSubstring("abcabcbb")) // 3
