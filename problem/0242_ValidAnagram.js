7ed23ce3be4d527941847739c80931f0dc843ca3
VanessaLin9 <vanessa7591@gmail.com>
2023-04-03 15:23:15 +0800

    sort and count solution

--- problem/0242_ValidAnagram.js
+++ problem/0242_ValidAnagram.js
@@ -11,24 +11,53 @@
  */
 
 // hash table
-var isAnagram = function(s, t) {
-  if(s.length !== t.length) return false
-  const hash = {}
-  for(let i in s){
-    if(hash[s[i]] === undefined) hash[s[i]] = 0
-    hash[s[i]] += 1
-  }
-  for(let j in t) {
-    if(hash[t[j]] === undefined) return false;
-    hash[t[j]] -= 1
-  }
-  for(let k in hash) {
-    if(hash[k] !== 0) return false
-  }
-  return true
-};
+// var isAnagram = function(s, t) {
+//   if(s.length !== t.length) return false
+//   const hash = {}
+//   for(let i in s){
+//     if(hash[s[i]] === undefined) hash[s[i]] = 0
+//     hash[s[i]] += 1
+//   }
+//   for(let j in t) {
+//     if(hash[t[j]] === undefined) return false;
+//     hash[t[j]] -= 1
+//   }
+//   for(let k in hash) {
+//     if(hash[k] !== 0) return false
+//   }
+//   return true
+// };
 
 // T: O(n)
 // S: O(n)
 
-console.log(isAnagram("rat","car"))
+// ---------------------------------------
+
+// Counting
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    const count = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 97]++;
+        count[t.charCodeAt(i) - 97]--;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) return false;
+    }
+    return true;
+};
+// T: O(n)
+// S: O(1) //固定只用length = 26的array
+
+// ---------------------------------------
+
+// sort
+
+// var isAnagram = function(s, t) {
+//     return s.split('').sort().join('') === t.split('').sort().join('');
+// };
+
+// TC: O(nlogn)
+// SC: O(n)
+
+console.log(isAnagram("rat","car")) //false
