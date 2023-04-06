08be669e8bfbcd53d71c41e0b7b9d249fa7f1c82
VanessaLin9 <vanessa7591@gmail.com>
2023-04-06 16:13:32 +0800

    two pointer

---
+++ medium-problem/0011_Container With Most Water.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+// two pointer
+var maxArea = function(height) {
+    let ans = 0
+    let l =0 ;
+    let r = height.length -1;
+    while(r>l){
+        ans = Math.max(ans, calculate(l, r))
+       if(height[r] > height[l]){
+           l++
+       } else if(height[r] < height[l]){
+           r--
+       } else {
+           l++;
+           r--;
+       }
+    }
+    return ans
+
+  function calculate(l, r){
+    let width = r - l;
+    let hei = Math.min(height[l], height[r])
+    return width * hei
+  }
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7])) //49
