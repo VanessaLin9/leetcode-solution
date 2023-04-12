070172237e0dc7b952939a5d00c00aa467757d28
VanessaLin9 <vanessa7591@gmail.com>
2023-04-12 13:17:03 +0800

    DP & map solution

--- problem/1480_Running Sum of 1d Array.js
+++ problem/1480_Running Sum of 1d Array.js
@@ -5,11 +5,34 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// DP
+var runningSum = function(nums) {
+    for(let i=1; i<nums.length; i++){
+        [nums[i-1], nums[i]] = [nums[i-1], nums[i]+nums[i-1]]
+    }
+    return nums
+};
+// TC: O(n)
+// MC: O(1)
+
+// map
+var runningSum = function(nums) {
+    return nums.map((curr, idx, arr) => {
+        return arr[idx] = idx === 0? curr: curr + arr[idx-1]
+    })
+};
+// TC: O(n)
+// MC: O(n)
+
+// reduce
 var runningSum = function(nums) {
     nums.reduce((a, b, idx, arr) => {
      return arr[idx] += a
     }, 0)
     return nums
 };
+// TC: O(n)
+// MC: O(1)
 
 console.log(runningSum([1,2,3,4])) // [ 1, 3, 6, 10 ]
