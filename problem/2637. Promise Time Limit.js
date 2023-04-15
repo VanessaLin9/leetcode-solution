31dadf79a02b00f84f76234ea648b8c15ee63f94
VanessaLin9 <vanessa7591@gmail.com>
2023-04-15 19:52:58 +0800

    promise

---
+++ problem/2637. Promise Time Limit.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+       const fnPromise = fn(...args)
+       const timer = new Promise((resolve, reject) => {
+           setTimeout(() => {reject('Time Limit Exceeded')}, t)
+       })
+
+       return Promise.race([fnPromise, timer])
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
