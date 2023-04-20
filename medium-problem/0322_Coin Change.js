930ff868904074d5690aebb9fa7d0e73a44c7928
VanessaLin9 <vanessa7591@gmail.com>
2023-04-20 15:44:41 +0800

    DP

---
+++ medium-problem/0322_Coin Change.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+const coinChange = (coins, amount) => {
+  const dp = Array(amount + 1).fill(Infinity); // This arr tells us how many coins we need for each amount.
+  dp[0] = 0; // To make 0, we need 0 coins.
+  for (let coin of coins) { // Check each coin
+    for (let i = coin; i <= amount; i++) { // Iterate through the entire amount from coin
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1); // Update minimum number of needed coins.
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount]; // If the last element is Infinity, then we cannot make the amount.
+};
