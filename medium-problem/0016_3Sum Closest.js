ce9a3ec618c1916379aee88f12f681c59d4b1739
VanessaLin9 <vanessa7591@gmail.com>
2023-04-29 17:02:56 +0800

    two pointer

---
+++ medium-problem/0016_3Sum Closest.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  if(nums.length <= 3) {
+    return nums.reduce((a, c) => a+c, 0)
+  }
+  nums.sort((a, b) => a-b)
+  let min = Infinity
+  for(let i =0; i<nums.length-2; i++) {
+    let l = i+1, r = nums.length-1
+    while(l < r) {
+      let sum = nums[i] + nums[l] + nums[r]
+      if(sum === target) {
+        return sum
+      }
+      if(Math.abs(sum-target) < Math.abs(min-target)) {
+        min = sum
+      }
+      if(sum < target) {
+        l++
+      } else {
+        r--
+      }
+    }
+  }
+  return min
+};
