90cbd3b6e4da77b50ab61fa090a2261bc247f5cf
VanessaLin9 <vanessa7591@gmail.com>
2023-05-07 22:27:39 +0800

    JS constructor

---
+++ problem/2665_Counter II.js
@@ -0,0 +1,57 @@
+// class
+
+class Counter {
+  constructor(init) {
+    this.init = init;
+    this.presentCount = init;
+  }
+
+  increment() {
+    this.presentCount += 1;
+    return this.presentCount;
+  }
+
+  decrement() {
+    this.presentCount -= 1;
+    return this.presentCount;
+  }
+
+  reset() {
+    this.presentCount = this.init;
+    return this.presentCount;
+  }
+}
+
+var createCounter = function(init) {
+  return new Counter(init);
+};
+
+// function
+var createCounter = function(init) {
+  let presentCount = init;
+
+  function increment() {
+    return ++presentCount;
+  }
+
+  function decrement() {
+      return --presentCount;
+  }
+
+  function reset() {
+      return (presentCount = init);
+  }
+
+  return { increment, decrement, reset };
+};
+
+// arrow function 
+
+var createCounter = function(init) {
+    let presentCount = init
+    return {
+        increment:()=> ++presentCount,
+        decrement:()=> --presentCount,
+        reset:()=> presentCount = init,
+    }
+};
