768e83eb7a28a237a95f4c63e13bfb301cf4191e
VanessaLin9 <vanessa7591@gmail.com>
2023-05-08 17:45:20 +0800

    match & replace

--- problem/0191_Number of 1 Bits.js
+++ problem/0191_Number of 1 Bits.js
@@ -6,13 +6,28 @@
  */
 
 // brute force
-var hammingWeight = function(n) {
-  let nBi = n.toString(2) + ''
-  let result = 0;
-  for(let i=0; i<=32; i++){
-    if(nBi[i] === "1") result ++;
-  }
-  return result
+// var hammingWeight = function(n) {
+//   let nBi = n.toString(2) + ''
+//   let result = 0;
+//   for(let i=0; i<=32; i++){
+//     if(nBi[i] === "1") result ++;
+//   }
+//   return result
+// };
+// 
+
+
+// RegEx 
+var hammingWeight = function(int) {
+    const str = int.toString(2)
+    return str === '0' ? 0 : (str.match(/1/g)).length; 
 };
 
-console.log(hammingWeight(00000000000000000000000000001011))
+// replace
+var hammingWeight = function(int) {
+    return int.toString(2).replaceAll("0", "").length;   //也可以用replace(/0/g, '') 
+};
+
+
+
+console.log(hammingWeight(00000000000000000000000000001011)) //3
