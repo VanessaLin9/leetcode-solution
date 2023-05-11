f9c5bee8b6e1b96813af8cbbec4b7f8c0cc62a1f
VanessaLin9 <vanessa7591@gmail.com>
2023-05-11 17:32:23 +0800

    DP

---
+++ medium-problem/0494_Target Sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+// DP
+var findTargetSumWays = function(nums, S) {
+  let sums = new Map([[0, 1]]);
+  
+  for (let num of nums) {
+    const next = new Map();
+    
+    for (let [sum, amount] of sums) {
+      const plus = sum + num;
+      const minus = sum - num;
+
+      next.set(plus, next.has(plus) ? next.get(plus) + amount : amount);
+      next.set(minus, next.has(minus) ? next.get(minus) + amount : amount);
+    }
+    
+    sums = next;
+  }
+  
+  return sums.has(S) ? sums.get(S) : 0;
+};
+
+// TC:O(n*s)
+// MC:O(n*s)
