cdc269dc35df5a9913039b26eabd531689daa32d
VanessaLin9 <vanessa7591@gmail.com>
2023-05-16 17:36:15 +0800

    sliding window

---
+++ medium-problem/0438_Find All Anagrams in a String.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+
+// sliding window
+var findAnagrams = function(s, p) {
+    const output = [];
+    const needChart = {};
+
+    for(let char of p){
+        if(char in needChart){
+            needChart[char] ++
+        } else {
+            needChart[char] = 1
+        }
+    }
+
+    let left = 0;
+    let right = 0;
+    let count = p.length;
+
+    while(right < s.length){
+        if(needChart[s[right]]>0)count --;
+        needChart[s[right]] --;
+        right ++;
+
+        if(count === 0) output.push(left)
+        if(right - left === p.length){
+            if(needChart[s[left]]>=0) count ++;
+            needChart[s[left]]++
+            left ++;
+        }
+    }
+    return output
+};
