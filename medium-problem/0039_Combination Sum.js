cc512596b23056e587aee7cb0adb018dc0b32246
VanessaLin9 <vanessa7591@gmail.com>
2023-05-19 12:57:27 +0800

    add some explain commit

--- medium-problem/0039_Combination Sum.js
+++ medium-problem/0039_Combination Sum.js
@@ -46,6 +46,7 @@ var combinationSum = function(candidates, target) {
   return result;
   
   function search(start, target){
+    // console.log(start, target) 全部的console在下面解釋
     if(target < 0) return;
     if(target === 0) return result.push(stack.slice());
     if(start === candidates.length) return;
@@ -58,5 +59,62 @@ var combinationSum = function(candidates, target) {
   }
 };
 
-
 console.log(combinationSum([2,3,6,7], 7)) //[[2,2,3],[7]]
+
+// 試圖理解所以跟著一步一步走一次
+//     stack
+// 0 7 [2]
+// 0 5 [2, 2] ------------------------------------> pop-> [2] start = 1
+// 0 3 [2, 2, 2] ----------------------------> pop -> [2, 2] start = 1
+// 0 1 [2, 2, 2, 2] ->pop -> [2, 2, 2] start =1
+// 0 -1 return
+// 1 1 [2, 2, 2, 3] -> pop -> [2, 2, 2] start =2
+// 1 -2 return
+// 2 1 [2, 2, 2, 6] ->pop -> [2, 2, 2] start =3
+// 2 -5 return
+// 3 1 [2, 2, 2, 7] -> pop -> [2, 2, 2] start =4
+// 3 -6 return
+// 4 1 start===length return
+// 1 3 [2, 2, 3] -> pop > [2, 2] start =2
+// 1 0 push([2, 2, 3]) return
+// 2 3 [2, 2, 6] -> pop -> [2, 2] start =3
+// 2 -3 return
+// 3 3 [2, 2, 7] -> pop -> [2, 2] start =4
+// 3 -4 return
+// 4 3 start ===length return
+// 1 5 
+// 1 2
+// 1 -1
+// 2 2
+// 2 -4
+// 3 2
+// 3 -5
+// 4 2
+// 2 5
+// 2 -1
+// 3 5
+// 3 -2
+// 4 5
+// 1 7
+// 1 4
+// 1 1
+// 1 -2
+// 2 1
+// 2 -5
+// 3 1
+// 3 -6
+// 4 1
+// 2 4
+// 2 -2
+// 3 4
+// 3 -3
+// 4 4
+// 2 7
+// 2 1
+// 2 -5
+// 3 1
+// 3 -6
+// 4 1
+// 3 7
+// 3 0
+// 4 7
