7f0f73f27f4dcb14080e2c5461e2f5476292dec8
VanessaLin9 <vanessa7591@gmail.com>
2023-05-24 21:14:54 +0800

    add set and binary search solution

--- problem/1539_Kth Missing Positive Number.js
+++ problem/1539_Kth Missing Positive Number.js
@@ -10,6 +10,22 @@
  * @param {number} k
  * @return {number}
  */
+
+// set
+var findKthPositive = function(arr, k) {
+    const set = new Set(arr)
+    let count = 0;
+    for(let i=1; i<=arr[arr.length-1]+k; i++){
+        if(!set.has(i)){
+            count ++
+            if(count === k) return i
+        }
+    }
+};
+// TC: O(n+k)
+// MC: O(n)
+
+
 //burst force
 var findKthPositive = function(arr, k) {
     let a = 0;
@@ -25,17 +41,48 @@ var findKthPositive = function(arr, k) {
     return -1
 };
 
-// 討論區看到解法
-// var findKthPositive = function(arr, k) {
-//     let prev = 0;
+// TC: O(n+k)
+// MC: O(1)
+
+// 討論區看到解法，算是liner search 的一種(?GPT說的)
+var findKthPositive = function(arr, k) {
+    let prev = 0;
     
-//     for(let i = 0; i < arr.length; i++) {
-//         const diff = arr[i] - prev - 1;
-//         if(k > diff) k -= diff;
-//         else return prev + k;
-//         prev = arr[i];
-//     }
-//     return prev + k     
-// };
+    for(let i = 0; i < arr.length; i++) {
+        const diff = arr[i] - prev - 1; // 當前的跟上一個應該相差1，超過的話是多的差額
+        if(k > diff) k -= diff;
+        else return prev + k;
+        prev = arr[i];
+    }
+    return prev + k     
+};
+
+// TC: O(n)
+// MC: O(1)
+
+// binary search
+
+//arr[i]   1 4 5 7 11
+//i+1      1 2 3 4 5  // if no number's missing, arr[i] should be i+1.
+//missing  0 2 2 3 6
+
+var findKthPositive = function(arr, k) {
+    let l =0;
+    let r = arr.length;
+    while(r>l){
+        let mid = l + Math.floor((r-l)/2)
+        console.log(mid, arr[mid])
+        if(arr[mid] -mid -1 <k){
+            l = mid+1
+        } else {
+            r = mid
+        }
+    }
+    return k + l
+};
+
+// TC: O(log n)
+// MC: O(1)
+
 
 console.log(findKthPositive([2,3,4,7,11], 5)) //9
