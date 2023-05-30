42876c78a8f2a052720b080ef8f60a92b89deea5
VanessaLin9 <vanessa7591@gmail.com>
2023-05-31 00:53:36 +0800

    brute force

---
+++ problem/0066_Plus One.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let num = digits.length-1
+    let flag = true
+    while(flag && num>=0){
+       let dig = digits[num] + 1
+       if(dig <10){
+           digits[num] = dig
+           flag = false
+       } else {
+           digits[num] = dig - 10
+           num --
+       }
+    }
+    flag ? digits.unshift(1): digits
+    return digits
+};
