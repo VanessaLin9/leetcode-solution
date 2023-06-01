b05338e0b80aedb2151ddc43352cd24db50f5b69
VanessaLin9 <vanessa7591@gmail.com>
2023-06-02 02:29:55 +0800

    add two pointer solution

--- problem/0905_Sort Array By Parity.js
+++ problem/0905_Sort Array By Parity.js
@@ -5,6 +5,29 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+//two pointer
+var sortArrayByParity = function(nums) {
+    let l =0, r=nums.length-1;
+    while(r>l){
+        if(odd(nums[l])){
+            l++
+        } else if(!odd(nums[r])){
+            r--
+        } else {
+            [nums[r], nums[l]] = [nums[l], nums[r]]
+        }
+    }
+    return nums
+};
+
+function odd(n){
+    return n%2 === 0
+}
+
+//TC: O(n)
+//MC: O(1)
+
 var sortArrayByParity = function(nums) {
     const arr = [...nums.filter(a => a%2 === 0), ...nums.filter(a => a%2 === 1)]
     return arr
