467c3267a552c33d9511c681101a2bf1ba3a68bd
VanessaLin9 <vanessa7591@gmail.com>
2023-06-15 16:15:12 +0800

    reduce

--- problem/0268_MissingNumber.js
+++ problem/0268_MissingNumber.js
@@ -113,5 +113,16 @@ var missingNumber = function(nums) {
 // T: O(n)
 // S: O(1)
 
+// _____________________________________________________________
+// 
+//reduce -AC 59 ms
+var missingNumber = function(nums) {
+    let sum = ((nums.length+1) * (nums.length))/2
+    let sum2 = nums.reduce((a, c) => a+c, 0)
+    return sum -sum2
+};
+// T: O(n)
+// S: O(1)
+
 console.log(missingNumber([9,6,4,2,3,5,7,0,1])) //8
 console.log(missingNumber([0,1])) //2
