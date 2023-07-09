7492096a6e9f0c1e0051984356988a7a57653365
VanessaLin9 <vanessa7591@gmail.com>
2023-07-09 22:17:21 +0800

    c# solution

--- problem/0009_Palindrome Number.js
+++ problem/0009_Palindrome Number.js
@@ -20,3 +20,13 @@ var isPalindrome = function(x) {
 };
 
 console.log(isPalindrome(10))
+
+// C#
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        var i = x.ToString().ToCharArray();
+        Array.Reverse(i);
+        return x.ToString() == new string(i);
+    }
+}
