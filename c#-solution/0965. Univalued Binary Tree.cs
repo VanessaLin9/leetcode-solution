11a7edeb36236bf21e029d2283053768bc590f74
VanessaLin9 <vanessa7591@gmail.com>
2023-07-12 22:19:01 +0800

    c# recursion

---
+++ c#-solution/0965. Univalued Binary Tree.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsUnivalTree(TreeNode root) {
+        bool isSam(TreeNode node, int val)
+        {
+            if(node == null) return true;
+            if(node.val != val) return false;
+            return isSam(node.left, val) && isSam(node.right, val);
+        }
+        return isSam(root, root.val);
+    }
+}
