f0281c21a10ff1a55bfeef2ca87642e065fa0607
VanessaLin9 <vanessa7591@gmail.com>
2023-07-18 22:23:07 +0800

    C# brute force

---
+++ c#-solution/0263. Ugly Number.cs
@@ -0,0 +1,12 @@
+// brute force
+public class Solution
+{
+  public bool IsUgly(int n)
+  {
+    if (n <= 0) return false;
+    while (n % 5 == 0) n /= 5;
+    while (n % 3 == 0) n /= 3;
+    while (n % 2 == 0) n /= 2;
+    return n == 1;
+  }
+}
