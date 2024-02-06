2d1a22ee6ab29006aeb1d218f602593236ae1e33
VanessaLin9 <vanessa7591@gmail.com>
2024-02-06 16:14:43 -0500

    C# dp solution

---
+++ c#-solution/0070. Climbing Stairs.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n<=2)
+        {
+            return n;
+        }
+        int[] step = new int [n+1];
+        for(int i=0; i<=n; i++){
+            if(i<=2)
+            {
+                step[i] = i;
+            } else {
+                step[i] = step[i-1] + step[i-2];
+            }
+        }
+        return step[n];
+    }
+}
