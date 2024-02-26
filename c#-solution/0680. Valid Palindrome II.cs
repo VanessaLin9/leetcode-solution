3dbfdbd534ba762b0b7b5c814388c7b526a5f07c
VanessaLin9 <vanessa7591@gmail.com>
2024-02-26 09:25:35 +0800

    c# solution

---
+++ c#-solution/0680. Valid Palindrome II.cs
@@ -0,0 +1,33 @@
+namespace DefaultNamespace;
+public class Solution {
+    public bool ValidPalindrome(string s) {
+        int start = 0;
+        int end = s.Length-1;
+        bool skipped = false;
+
+        return IsValidPalindrome(s, start, end, skipped);
+    }
+
+    private bool IsValidPalindrome(string str, int start,int end,bool skipped)
+    {
+        while(end > start)
+        {
+            if(str[start] == str[end])
+            {
+                start ++;
+                end --;
+            } 
+            else {
+                if(skipped)
+                {
+                    return false;
+                }
+                else {
+                    return IsValidPalindrome(str, start +1, end, true) ||
+                           IsValidPalindrome(str, start, end -1, true) ;
+                }
+            }
+        }
+        return true;
+    }
+}
