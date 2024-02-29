c038873a2fb86246073a6a2f668fe7b4a02b89e9
VanessaLin9 <vanessa7591@gmail.com>
2024-02-29 09:41:20 +0800

    new 485 with 2 C# solution

---
+++ c#-solution/0485. Max Consecutive Ones.cs
@@ -0,0 +1,26 @@
+namespace DefaultNamespace;
+
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int temp = 0;
+        for(int i =0; i<nums.Length; i++){
+            if(nums[i] == 1){
+                temp ++;
+                ans = Math.Max(ans, temp);
+            } else {
+                temp = 0;
+            }
+        }
+        return ans;
+    }
+}
+
+// split solution
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        string s = String.Join("", nums);
+        string [] strs = s.Split('0');
+        return (from str in strs select str.Length).Max();
+    }
+}
