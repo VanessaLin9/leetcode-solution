c70b731e81436aaee7269ccb2d7c09cea1cf3dc8
VanessaLin9 <vanessa7591@gmail.com>
2024-03-03 20:11:05 +0800

    C# solution

---
+++ c#-solution/0733. Flood Fill.cs
@@ -0,0 +1,31 @@
+namespace DefaultNamespace;
+
+public class Solution {
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color) {
+        var origin = image[sr][sc];
+        if(origin == color) return image;
+        
+        Fill(sr, sc);
+        return image;
+
+        void Fill(int r, int c){
+            if(image[r][c] != origin) return;
+            image[r][c] = color;
+            if(r+1<image.Length){
+                Fill(r+1, c);
+            }
+            if(r-1 >= 0){
+                Fill(r-1, c);
+            }
+            if(c+1<image[r].Length){
+                Fill(r, c+1);
+            }
+            if(c-1>=0){
+                Fill(r, c-1);
+            }
+            return;
+        }
+    }
+}
+// time complexity: O(n*m)
+// space complexity: O(n*m)
