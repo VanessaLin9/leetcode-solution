ab68952cc186080707b11ee59e7fc6c488884b5c
vanessa <vanessa.lin@titansoft.com.sg>
2024-08-24 21:15:08 +0800

    add 594 c# solution

---
+++ c#-solution/0594. Longest Harmonious Subsequence.cs
@@ -0,0 +1,28 @@
+namespace DefaultNamespace;
+
+public class Solution {
+    public int FindLHS(int[] nums)
+    {
+        var hash = new Dictionary<int, int>();
+        foreach (var i in nums)
+        {
+            if (!hash.ContainsKey(i))
+            {
+                hash[i] = 0;
+            }
+            hash[i]++;
+        }
+
+        var result = 0;
+        foreach (var j in hash.Keys)
+        {
+            if (hash.ContainsKey(j + 1))
+            {
+                var temp = hash[j] + hash[j + 1];
+                result = Math.Max(temp, result);
+            }
+        }
+
+        return result;
+    }
+}
