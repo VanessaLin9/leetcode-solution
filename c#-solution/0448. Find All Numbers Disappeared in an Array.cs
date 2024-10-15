37e54ba7fb787b72b8b43ebec7fe60287d05f3f4
VanessaLin9 <vanessa7591@gmail.com>
2024-10-15 10:41:08 +0800

    sortedset solution

--- c#-solution/0448. Find All Numbers Disappeared in an Array.cs
+++ c#-solution/0448. Find All Numbers Disappeared in an Array.cs
@@ -18,6 +18,7 @@ public class Solution {
 // time complexity: O(n)
 // space complexity: O(n)
 
+//-----------------------------------------------------------------------------------------------
 // HashSet solution
 public class Solution {
     public IList<int> FindDisappearedNumbers(int[] nums) {
@@ -32,9 +33,41 @@ public class Solution {
 // time complexity: O(n)
 // space complexity: O(n)
 
+//-----------------------------------------------------------------------------------------------
 // In-place solution
 public class Solution {
     public IList<int> FindDisappearedNumbers(int[] nums) {
         return Enumerable.Range(1, nums.Length).Except(nums).ToList();
     }
 }
+//-----------------------------------------------------------------------------------------------
+// SortedSet solution
+public class Solution {
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        // 創建 SortedSet 將輸入的數字去重並排序
+        var set = new SortedSet<int>(nums);
+        Console.WriteLine("Set contents: " + string.Join(",", set));
+        
+        var ans = new List<int>();
+        int p = 1;
+
+        // 使用 foreach 遍歷 set 中的數字
+        foreach(var i in set) {
+            // 當前的 p 和 set 中的數字 i 不匹配，則 p 是缺失的數字
+            while(p < i) {
+                ans.Add(p);
+                p++;
+            }
+            p++;  // 當 p == i，移到下一個數字
+        }
+
+        // 如果還有數字沒遍歷到，將它們加到結果中
+        while(p <= nums.Length) {
+            ans.Add(p);
+            p++;
+        }
+        return ans;
+    }
+}
+// time complexity: O(nlogn)
+// space complexity: O(n)
