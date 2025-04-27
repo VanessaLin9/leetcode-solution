239ab550d22fa5bc6ce49a433a3c6d12c9a7e34d
VanessaLin9 <vanessa7591@gmail.com>
2025-04-27 22:11:47 +0800

    add 344 c#  solution

---
+++ c#-solution/0344. Reverse String.cs
@@ -0,0 +1,15 @@
+// 0344 Reverse String
+// https://leetcode.com/problems/reverse-string/description/
+// Tag: Array, Two Pointers, String, Easy
+
+public class Solution {
+    public void ReverseString(char[] s) {
+        int haf = s.Length/2;
+
+        for(int i=0; i<haf; i++){
+            var c = s[i];
+            s[i] = s[s.Length -1 - i];
+            s[s.Length -1 -i] = c;
+        }
+    }
+}
