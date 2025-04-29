2a20806df874e0ba8adcc713c3d048f64d0e8bfd
VanessaLin9 <vanessa7591@gmail.com>
2025-04-29 19:50:58 +0800

    add new solution

--- c#-solution/0268. MissingNumber.cs
+++ c#-solution/0268. MissingNumber.cs
@@ -12,3 +12,33 @@ public class Solution {
         return nums.Length ;
     }
 }
+// TC: O(n^2)
+// SC: O(1)
+
+// hashset
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        var set = new HashSet<int>(nums);
+        for(int i=0; i<nums.Length; i++){
+            if(!set.Contains(i)) return i;
+        }
+        return nums.Length ;
+    }
+}
+// TC: O(n)
+// SC: O(n)
+
+// math
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int sum = nums.Length;
+        int curr = 0;
+        for(int i=0; i<nums.Length; i++){
+            curr += nums[i];
+            sum += i;
+        }
+        return sum - curr;
+    }
+}
+// TC: O(n)
+// SC: O(1)
