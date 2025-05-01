c2b21f634bd00922ecb8ecbc72c6bea76c664b1a
VanessaLin9 <vanessa7591@gmail.com>
2025-05-01 23:44:43 +0800

    add 1 new solution 680

--- c#-solution/0680. Valid Palindrome II.cs
+++ c#-solution/0680. Valid Palindrome II.cs
@@ -34,3 +34,30 @@ public class Solution {
         return true;
     }
 }
+
+// one run no recursion
+public class Solution {
+    
+    public bool ValidPalindrome(string s) {
+        int left = 0, right = s.Length - 1;
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                return IsPalindrome(s, left + 1, right) || IsPalindrome(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+
+    private bool IsPalindrome(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
