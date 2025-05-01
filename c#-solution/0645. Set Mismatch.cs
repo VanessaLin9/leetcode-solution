82044f4d7c43ece4ce3191f747dba05990ab6efa
VanessaLin9 <vanessa7591@gmail.com>
2025-05-02 03:05:00 +0800

    add 1 improving  solution in 645

--- c#-solution/0645. Set Mismatch.cs
+++ c#-solution/0645. Set Mismatch.cs
@@ -7,7 +7,7 @@ public class hashSetSolution {
         var set = new HashSet<int>();
         var result = new int [2];
         foreach(int n in nums){
-            if(!set.Add(n)){
+            if(!set.Add(n)){ // 不論有沒有加入成功都會試著加入，成功則返回true，失敗則返回false
                 result[0] = n;
             }
         }
@@ -22,3 +22,22 @@ public class hashSetSolution {
 // TC: O(n)
 // SC: O(n)
 // Note: The solution uses a HashSet to track the numbers seen so far.
+
+public class Solution {
+    public int[] FindErrorNums(int[] nums) {
+        var set = new HashSet<int>();
+        var result = new int [2];
+        int numsSum = 0;
+        foreach(int n in nums){
+            if(!set.Add(n)){
+                result[0] = n;
+            } else {
+                numsSum += n;
+            }
+        }
+        int l = nums.Length;
+        int expectedSum = (l +1)*l/2; // 等差數列求和公式: (a1 + an) * n / 2
+        result[1] = expectedSum - numsSum;
+        return result;
+    }
+}
