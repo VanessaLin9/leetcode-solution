d78497beb227e5d68712fe322dc6c7623d4e130d
VanessaLin9 <vanessa7591@gmail.com>
2025-05-12 17:57:17 +0800

    simple solution

--- c#-solution/0876. Middle of the Linked List.cs
+++ c#-solution/0876. Middle of the Linked List.cs
@@ -34,3 +34,16 @@ public class Solution {
     }
     
 }
+
+
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        var fast = head;
+        var slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
