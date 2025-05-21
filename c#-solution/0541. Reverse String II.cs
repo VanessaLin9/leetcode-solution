b402a1e987018f72e752fb3f2a0e577849d46773
VanessaLin9 <vanessa7591@gmail.com>
2025-05-22 02:23:14 +0800

    add 541 string builder solution

--- c#-solution/0541. Reverse String II.cs
+++ c#-solution/0541. Reverse String II.cs
@@ -21,3 +21,27 @@ public class Solution {
 }
 // TC: O(n)
 // SC: O(n)
+
+// string builder
+public class Solution {
+    public string ReverseStr(string s, int k) {
+    var sb = new StringBuilder();
+        var p = 0;
+        while(p<s.Length){
+            int end = Math.Min(s.Length, p+k);
+            for(int i=end-1; i>=p; i--){
+                sb.Append(s[i]);
+            }
+
+            int remainStart = end;
+            int remainEnd = Math.Min(p + 2*k, s.Length);
+            for(int j=remainStart; j<remainEnd; j++){
+                sb.Append(s[j]);
+            }
+
+            p+= 2*k;
+        }
+
+        return sb.ToString();
+    }
+}
