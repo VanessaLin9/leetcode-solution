411fa5131389e6e8190909841d3b02b560e031e7
VanessaLin9 <vanessa7591@gmail.com>
2025-05-26 16:35:35 +0800

    add 1 504 js solution

--- problem/0504_Base 7.js
+++ problem/0504_Base 7.js
@@ -1,3 +1,7 @@
+// 504. Base 7
+// https://leetcode.com/problems/base-7/
+// Tag: Math, String, Easy
+
 /**
  * @param {number} num
  * @return {string}
@@ -6,4 +10,18 @@ var convertToBase7 = function(num) {
   return num.toString(7)
 };
 
+var convertToBase7 = function(num) {
+    if(num == 0) return "0";
+    let isNegative = num < 0;
+    let result = "";
+    num = Math.abs(num);
+    while(num > 0){
+        let temp = num %7;
+        result = temp + result;
+        num = (num - temp)/7;
+    }
+    return isNegative ? "-" + result:result;
+};
+
+// --- for testing ---
 console.log(convertToBase7(100)) //202
