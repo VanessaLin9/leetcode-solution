a709a7622e561da543885327538d26c3f9492284
VanessaLin9 <vanessa7591@gmail.com>
2025-05-28 18:51:25 +0800

    Add 905 concat solution and comment

--- c#-solution/0905. Sort Array By Parity.cs
+++ c#-solution/0905. Sort Array By Parity.cs
@@ -26,8 +26,14 @@ public class Solution {
         }
         return nums;
     }
+    // TC: O(n)
+    // SC: O(1)
     
-    // AddRange approach 
+
+    
+    // AddRange approach
+    // 內部實作會先根據你要加的元素數量「擴容一次」，然後逐一填入。
+    // 因為 List 的 Capacity 是可以提前設定的，所以可以大幅減少記憶體重分配與拷貝次數。
     public int[] SortArrayByParity(int[] nums) {
         List<int> evenNumbers = nums.Where(x => x%2 == 0).ToList();
         List<int> oddNumbers = nums.Where(x => x%2 == 1).ToList();
@@ -35,4 +41,23 @@ public class Solution {
         evenNumbers.AddRange(oddNumbers);
         return evenNumbers.ToArray();
     }
+    // TC: O(n)
+    // SC: O(n)
+
+
+    // LINQ approach
+    // 效能較差
+    // 因為 LINQ 的 Where 和 Concat 方法會產生新的 IEnumerable 物件，並且在 ToArray() 時需要遍歷兩次。
+    // GC（垃圾回收）也會因為這些額外的物件而增加負擔。
+    // 這樣的實作方式會增加額外的記憶體使用量，並且在處理大數據時可能會導致效能下降，雖然TIME和SPACE的複雜度仍然是 O(n) 和 O(n)，
+    // 但實際上會比兩個 List 的 AddRange 方法更慢。
+    // 但這種寫法更簡潔，易於閱讀和維護。
+    public int[] SortArrayByParity(int[] nums) {
+        return nums.Where(x => x%2 ==0)
+        .Concat(nums.Where(y => y%2 != 0))
+        .ToArray();
+    }
+    // TC: O(n)
+    // SC: O(n)
+
 }
